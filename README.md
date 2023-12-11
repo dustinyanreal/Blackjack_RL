21048e8f7fc72cbd294a92a695a238876426ad12
Dustin Yan <doostin@dustins-mbp.lan>
2023-12-10 20:01:41 -0800

    added README info

--- README.md
+++ README.md
@@ -2,15 +2,38 @@
 ## Project Summary
 <!-- Around 200 Words -->
 <!-- Cover (1) What problem you are solving, (2) Who will use this RL module and be happy with the learning, and (3) a brief description of the results -->
+In the world of gambling, blackjack is a very popular card game. We propose an agent that can try and beat the casino by earning a lot of reward(money). The game is mostly luck, but with a bit a statistics, the odds for a person to beat the casino becoming slightly higher.
+
+The dealer/casino will have a simple strategy of just hitting(getting more cards) until it reaches either a number that is greater than 21, or until it reaches a number that we consider a valid stopping point. For a valid stopping point for a dealer/casino, it will stop when its cards add up to x, where 17 <= x >= 21. We also define that if a dealer gets a soft 17 i.e (A,6), the dealer will continue.
+
+For the agent, we will have it randomly pick options: Double, Hit, Stand. It will learn when to stop or when to hit, but if it reaches a number greater than 21, the agent losses. The agent will also have a little bit of a tricky strategy where it will count cards and understand the propabilities of potential cards to pop up. With "coutning cards", the agent will pick it's actions accordingly.
+
+If you're an passionate blackjack player and want an Agent to count cards and find the best move to play to earn a lot of money! Then this is the module for you!
 
 ## State Space
 <!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+The observation space will consists of a 3-tuple containing: the player's current sum, the value of the dealer's one showing card (1-10 where 1 is an ace), and whether the player holds a usuable ace(0,1)
 
 ## Action Space
 <!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+The action space will indicate the action the player takes. There are 3 options that a player can take.
+
+    0: stand
+    1: hit
+    2: double
 
 ## Rewards
 <!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+Since the goal of blackjack is to beat the dealer by getting close to 21, we will have 3 normal rewards and add 2 extra rewards for the double action.
+
+If we don't double:
+    +1.0 if we beat the dealer
+    0 if we go even with the dealer
+    -1.0 if we lose to the dealer
+If we double:
+    +2.0 if we beat the dealer
+    0 if we go even with the dealer <!-- The same as wehn we don't double -->
+    -2.0 if we lose to the dealer
 
 ## RL Algorithm 
 
@@ -19,6 +42,9 @@
 
 ## Episode End [if applicable]
 <!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+For this environment, we will end the episode if one of the followin occur:
+    1. If we have -100 as our reward, we don't want to lose too much money!
+    2. If the episode length is greater than 500, we don't want to sit at a blackjack table forever!
 
 ## Results
 
--- intro-custom-env-and-ray/blackjack_environment.py
+++ intro-custom-env-and-ray/blackjack_environment.py
@@ -1,8 +1,24 @@
 import numpy as np
 import pygame
+from typing import Optional
+from numpy import random
 
 import gymnasium as gym
 from gymnasium import spaces
+
+original_deck = [1,1,1,1, #A
+        2,2,2,2,
+        3,3,3,3,
+        4,4,4,4,
+        5,5,5,5,
+        6,6,6,6,
+        7,7,7,7,
+        8,8,8,8,
+        9,9,9,9,
+        10,10,10,10, #10
+        10,10,10,10, #J
+        10,10,10,10, #Q
+        10,10,10,10] #K
 
 deck = [1,1,1,1,
         2,2,2,2,
@@ -18,17 +34,30 @@ deck = [1,1,1,1,
         10,10,10,10, #Q
         10,10,10,10] #K
 
-def check_if_win(a, b):
+def check_shuffle():
+    shuffle = False
+    if (len(deck) < int(len(original_deck)/2)):
+        if len(deck) <= (len(original_deck) * 1/4):
+            shuffle = True
+        else:
+            x = random.randint(100)
+            if x > 75:
+                shuffle = True
+    return shuffle
+
+def compare(a, b):
     return float(a > b) - float(a < b)
 
 def draw_card(np_random):
     card = int(np_random.choice(deck))
-    return card
+    shuffle = check_shuffle()
+    return card, shuffle
 
 def draw_hand(np_random):
     card1 = int(np_random.choice(deck))
     card2 = int(np_random.choice(deck))
-    return [card1, card2]
+    shuffle = check_shuffle()
+    return [card1, card2], shuffle
 
 def is_ace(hand):
     return 1 in hand and sum(hand) + 10 <= 21
@@ -47,7 +76,7 @@ def score(hand):
 def is_blackjack(hand):
     return sorted(hand) == [1, 10]
 
-class BlackJackEnv():
+class BlackJackEnv(gym.Env):
     def __init__(self, natural=False):
 
         self.action_space = spaces.Discrete(3)
@@ -57,19 +86,29 @@ class BlackJackEnv():
         self.natural = natural
 
     def step(self, action):
+        shuffle_next_turn = False
         assert self.action_space.contains(action)
         if action == 2: #double
             terminated = True
-            self.player.append(draw_card(self.np_random))
+            card, shuffle = draw_card(self.np_random)
+            if shuffle:
+                shuffle_next_turn = True
+            self.player.append(card)
             if is_bust(self.player):
                 reward = -2.0
             else:
                 while sum_hand(self.dealer) < 17:
-                    self.dealer.append(draw_card(self.np_random))
-                reward = check_if_win(score(self.player), score(self.dealer))
+                    card, shuffle = draw_card(self.np_random)
+                    if shuffle:
+                        shuffle_next_turn = True
+                    self.dealer.append(card)
+                reward = compare(score(self.player), score(self.dealer))
                 reward *= 2
         elif action == 1: #hit
-            self.player.append(draw_card(self.np_random))
+            card, shuffle = draw_card(self.np_random)
+            if shuffle:
+                shuffle_next_turn = True
+            self.player.append(card)
             if is_bust(self.player):
                 reward = -1.0
             else:
@@ -78,14 +117,25 @@ class BlackJackEnv():
         elif action == 0: #stand
             terminated = True
             while sum_hand(self.dealer) < 17:
-                self.dealer.append(draw_card(self.np_random))
-            reward = check_if_win(score(self.player), score(self.dealer))
+                card, shuffle = draw_card(self.np_random)
+                if shuffle:
+                    shuffle_next_turn = True
+                self.dealer.append(card)
+            reward = compare(score(self.player), score(self.dealer))
         else: #blackjack
             reward = 1.5
+
+        if shuffle_next_turn:
+            self.reset()
 
     def __get_obs(self):
         return (sum_hand(self.player), self.dealer[0], is_ace(self.player))
     
-    def reset(self):
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        super().reset(seed=seed)
         self.dealer = draw_hand(self.np_random)
         self.player = draw_hand(self.np_random)
+
+        _, dealer_card_value, _ = self.__get_obs()
+
+        deck = original_deck
